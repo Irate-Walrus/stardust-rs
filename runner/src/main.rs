4afc391711a0540ca9c0348c55912e271791dec2
Irate-Walrus <71628949+Irate-Walrus@users.noreply.github.com>
2024-10-02 06:23:20 +0000

    feat: use rwx memory on heap for runner

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,15 +1,12 @@
 use std::mem;
+use virtual_memory::*;
 
-// we do this trick because otherwise only the reference is in the .text section
-const SHELLCODE_BYTES: &[u8] = include_bytes!("../../target/stardust.bin");
-const SHELLCODE_LENGTH: usize = SHELLCODE_BYTES.len();
-
-#[no_mangle]
-#[link_section = ".text"]
-static SHELLCODE: [u8; SHELLCODE_LENGTH] = *include_bytes!("../../target/stardust.bin");
+const SHELLCODE: &[u8] = include_bytes!("../../target/stardust.bin");
 
 fn main() {
-    let exec_shellcode: extern "C" fn() -> ! =
-        unsafe { mem::transmute(&SHELLCODE as *const _ as *const ()) };
-    exec_shellcode();
+    let mut memory = VirtualMemory::new(SHELLCODE.len()).expect("failed to allocate rwx memory");
+    memory.copy_from_slice(SHELLCODE);
+
+    let exec: extern "C" fn() -> ! = unsafe { mem::transmute(memory.as_ptr()) };
+    exec();
 }
