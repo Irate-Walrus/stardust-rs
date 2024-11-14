95a6a1ff11bfce5442d894c94d8627a430ae25fc
Irate-Walrus <71628949+Irate-Walrus@users.noreply.github.com>
2024-11-14 11:30:30 +0000

    chore: simplify djb2 hash I probably should make a testing crate for this but can't be bothered

--- djb2_macro/src/lib.rs
+++ djb2_macro/src/lib.rs
@@ -1,21 +1,25 @@
 extern crate proc_macro;
+
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::parse_macro_input;
 
-lazy_static::lazy_static! {
-    static ref DJB2_MAGIC: u32 = 5381;
-}
+const DJB2_MAGIC_KEY: u32 = 5381;
 
 #[proc_macro]
 pub fn define_djb2_hash_fn(input: TokenStream) -> TokenStream {
     let func_name = parse_macro_input!(input as syn::Ident);
-    let random: u32 = *DJB2_MAGIC;
+    let hash: u32 = DJB2_MAGIC_KEY;
     let output = quote! {
         pub fn #func_name(data: &[u8]) -> u32 {
-            let mut hash: u32 = #random; // Start with a u32 hash
+            let mut hash: u32 = #hash; // Start with a u32 hash
             for &byte in data {
-                hash = (hash << 5).wrapping_add(hash).wrapping_add(byte as u32); // hash * 33 + byte
+                if byte == 0x0 {
+                    continue;
+                }
+                hash = (hash << 5)
+                .wrapping_add(hash)
+                .wrapping_add(byte as u32); // hash * 33 + byte
             }
             hash
         }
@@ -43,8 +47,11 @@ pub fn djb2_hash(input: TokenStream) -> TokenStream {
 }
 
 fn djb2_hash_u8(data: &[u8]) -> u32 {
-    let mut hash: u32 = *DJB2_MAGIC; // Start with a u32 hash
+    let mut hash: u32 = DJB2_MAGIC_KEY; // Start with a u32 hash
     for &byte in data {
+        if byte == 0x0 {
+            continue;
+        }
         hash = (hash << 5).wrapping_add(hash).wrapping_add(byte as u32); // hash * 33 + byte
     }
     hash
